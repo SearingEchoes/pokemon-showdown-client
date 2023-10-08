40152bbff85a587a50b24195d83157e42c768735
level2man <143778622+level2man@users.noreply.github.com>
2023-10-08 16:11:52 -0400

    Update battle-dex.ts

--- src/battle-dex.ts
+++ src/battle-dex.ts
@@ -873,7 +873,7 @@ const Dex = new class implements ModdedDex {
 		if (mod && (type !== '???')) {
 			return `<img src="${this.modResourcePrefix}${mod}/sprites/types/${toID(type)}.png" alt="${type}" class="pixelated${b ? ' b' : ''}" />`;
 		} else {
-			return `<img src="${Dex.resourcePrefix}sprites/types/${sanitizedType}png" alt="${type}" height="14" width="32" class="pixelated${b ? ' b' : ''}" />`;
+			return `<img src="${Dex.resourcePrefix}sprites/types/${sanitizedType}.png" alt="${type}" height="14" width="32" class="pixelated${b ? ' b' : ''}" />`;
 		}
 	}
 
